904eba27a05beb8b202bab4c5495fb3168c9046f
Cesar <3005614070101@ingeniera.usac.edu.gt>
2021-04-30 11:22:55 -0600

    Mostrar productos y agregarlos al carrito

--- Frontend/funciones/productos.js
+++ Frontend/funciones/productos.js
@@ -2,8 +2,6 @@ const ruta = "http://localhost:4000/producto";
 let plantillaHTML = "";
 
 function cargarProductos() {
-  const body_productos = document.getElementById("body-productos");
-
   fetch(ruta, {
     method: "GET",
     headers: {
@@ -17,202 +15,139 @@ function cargarProductos() {
       let filas = "";
 
       productos.forEach((producto) => {
-        filas += `\n<tr>
-        <td class="cell">${producto.id}</td>
-        <td class="cell">${producto.nombre}</td>
-        <td class="cell">${producto.correo}</td>
-        <td class="cell">${producto.edad}</td>
-        <td class="cell">${producto.fecha_nacimiento}</td>
-        </tr>`;
+        filas += `\n<div class="col-6 col-md-4 col-xl-3 col-xxl-2">
+        <div class="app-card app-card-doc shadow-sm h-100">
+          <div class="app-card-thumb-holder p-3">
+            <div class="app-card-thumb">
+              <img
+                class="thumb-image"
+                src="${producto.imagen}"
+                alt=""
+              />
+            </div>
+            <a class="app-card-link-mask" href="#file-link"></a>
+          </div>
+          <div class="app-card-body p-3 has-card-actions">
+            <h4 class="app-doc-title truncate mb-0">
+              <a href="#file-link">${producto.nombre}</a>
+            </h4>
+            <div class="app-doc-meta">
+              <ul class="list-unstyled mb-0">
+                <li><span>Precio:</span> Q${producto.precio}</li>
+                <li><span>Descripción:</span> ${producto.descripcion}</li>
+                <li><span>Código:</span> ${producto.id}</li>
+              </ul>
+            </div>
+            <!--//app-doc-meta-->
+            <a onclick="agregarCarrito(${producto.id})">
+              <div class="app-card-actions">
+                <div class="dropdown">
+                  <div
+                    class="dropdown-toggle no-toggle-arrow"
+                    data-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <svg
+                      version="1.1"
+                      id="Layer_1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlns:xlink="http://www.w3.org/1999/xlink"
+                      x="0px"
+                      y="0px"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 40 36"
+                      style="enable-background: new 0 0 40 36"
+                      xml:space="preserve"
+                    >
+                      <g id="Page-1_4_" sketch:type="MSPage">
+                        <g
+                          id="Desktop_4_"
+                          transform="translate(-84.000000, -410.000000)"
+                          sketch:type="MSArtboardGroup"
+                        >
+                          <path
+                            id="Cart"
+                            sketch:type="MSShapeGroup"
+                            class="st0"
+                            d="M94.5,434.6h24.8l4.7-15.7H92.2l-1.3-8.9H84v4.8h3.1l3.7,27.8h0.1
+         c0,1.9,1.8,3.4,3.9,3.4c2.2,0,3.9-1.5,3.9-3.4h12.8c0,1.9,1.8,3.4,3.9,3.4c2.2,0,3.9-1.5,3.9-3.4h1.7v-3.9l-25.8-0.1L94.5,434.6"
+                          />
+                        </g>
+                      </g>
+                    </svg>
+                  </div>
+                  <!--//dropdown-toggle-->
+                </div>
+                <!--//dropdown-->
+              </div>
+            </a>
+            <!--//app-card-actions-->
+          </div>
+          <!--//app-card-body-->
+        </div>
+        <!--//app-card-->
+      </div>`;
       });
 
       $("#body-productos").empty();
       $("#body-productos").append(filas);
 
-      plantillaHTML = `<!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-      
-          <title>Reporte productos</title>
-      
-          <link rel="icon" href="./images/favicon.png" type="image/x-icon" />
-      
-          <style>
-            body {
-              font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
-              text-align: center;
-              color: #777;
-            }
-      
-            body h1 {
-              font-weight: 300;
-              margin-bottom: 0px;
-              padding-bottom: 0px;
-              color: #000;
-            }
-      
-            body h3 {
-              font-weight: 300;
-              margin-top: 10px;
-              margin-bottom: 20px;
-              font-style: italic;
-              color: #555;
-            }
-      
-            body a {
-              color: #06f;
-            }
-      
-            .invoice-box {
-              max-width: 800px;
-              margin: auto;
-              padding: 30px;
-              border: 1px solid #eee;
-              box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-              font-size: 16px;
-              line-height: 24px;
-              font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
-              color: #555;
-            }
-      
-            .invoice-box table {
-              width: 100%;
-              line-height: inherit;
-              text-align: left;
-              border-collapse: collapse;
-            }
-      
-            .invoice-box table td {
-              padding: 5px;
-              vertical-align: top;
-            }
-      
-            .invoice-box table tr td:nth-child(2) {
-              text-align: right;
-            }
-      
-            .invoice-box table tr.top table td {
-              padding-bottom: 20px;
-            }
-      
-            .invoice-box table tr.top table td.title {
-              font-size: 45px;
-              line-height: 45px;
-              color: #333;
-            }
-      
-            .invoice-box table tr.information table td {
-              padding-bottom: 40px;
-            }
-      
-            .invoice-box table tr.heading td {
-              background: #eee;
-              border-bottom: 1px solid #ddd;
-              font-weight: bold;
-            }
-      
-            .invoice-box table tr.details td {
-              padding-bottom: 20px;
-            }
-      
-            .invoice-box table tr.item td {
-              border-bottom: 1px solid #eee;
-            }
-      
-            .invoice-box table tr.item.last td {
-              border-bottom: none;
-            }
-      
-            .invoice-box table tr.total td:nth-child(2) {
-              border-top: 2px solid #eee;
-              font-weight: bold;
-            }
-      
-            @media only screen and (max-width: 600px) {
-              .invoice-box table tr.top table td {
-                width: 100%;
-                display: block;
-                text-align: center;
-              }
-      
-              .invoice-box table tr.information table td {
-                width: 100%;
-                display: block;
-                text-align: center;
-              }
-            }
-          </style>
-        </head>
-      
-        <body>
-          <div class="invoice-box">
-            <table>
-              <tr class="top">
-                <td colspan="2">
-                  <table>
-                    <tr>
-                      <td class="title">
-                        <h4>Hospital USAC</h4>
-                      </td>
-      
-                      <td>Fecha creación: ${new Date().toLocaleDateString("es-US")}<br /></td>
-                    </tr>
-                  </table>
-                </td>
-              </tr>
-      
-              <tr class="information">
-                <td colspan="2">
-                  <table>
-                    <tr>
-                      <td>
-                        Sparksuite, Inc.<br />
-                        12345 Sunny Road<br />
-                        Sunnyville, TX 12345
-                      </td>
-      
-                      <td>
-                        USAC.<br />
-                        Cesar<br />
-                        cesar@example.com
-                      </td>
-                    </tr>
-                  </table>
-                </td>
-              </tr>
-            </table>
-      
-            <table>
-              <tr class="heading">
-                <td>ID</td>
-                <td>Nombre</td>
-                <td>Correo</td>
-                <td>Edad</td>
-                <td>Fecha nacimiento</td>
-              </tr>`;
-
-      productos.forEach((producto) => {
-        plantillaHTML += `\n<tr class="item">
-                <td>${producto.id}</td>
-                <td>${producto.nombre}</td>
-                <td>${producto.correo}</td>
-                <td>${producto.edad}</td>
-                <td>${producto.fecha_nacimiento}</td>
-              </tr>\n`;
-      });
-
-      plantillaHTML += `</table>
-          </div>
-        </body>
-      </html>`;
-
-      console.log(plantillaHTML);
+      localStorage.setItem("productos", JSON.stringify(productos));
     })
     .catch((err) => console.log(err));
 }
 
-function generarReporte() {
-  html2pdf().from(plantillaHTML).toPdf().save("reporte_productos.pdf");
+async function agregarCarrito(id_producto) {
+  let carrito = await localStorage.getItem("carrito");
+
+  if (!carrito) {
+    carrito = [{ id: id_producto, cantidad: 1 }];
+    console.log(carrito);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    Swal.fire({
+      text: "Agregado al carrito",
+      toast: true,
+      icon: "success",
+      timer: 2000,
+      showConfirmButton: false,
+      position: 'bottom-right'
+    });
+    return;
+  }
+
+  carrito = await JSON.parse(carrito);
+  let existe = false;
+
+  carrito.forEach((producto) => {
+    if (producto.id == id_producto) {
+      producto.cantidad++;
+      console.log(carrito);
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+      existe = true;
+      Swal.fire({
+        text: "Agregado al carrito",
+        toast: true,
+        icon: "success",
+        timer: 2000,
+        showConfirmButton: false,
+        position: 'bottom-right'
+      });
+      return;
+    }
+  });
+
+  if (!existe) {
+    carrito.push({ id: id_producto, cantidad: 1 });
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    Swal.fire({
+      text: "Agregado al carrito",
+      toast: true,
+      icon: "success",
+      timer: 2000,
+      showConfirmButton: false,
+      position: 'bottom-right'
+    });
+    console.log(carrito);
+  }
 }
